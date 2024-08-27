042fb36a9a92d721740259d2805babb78e512ed9
Julien Deplat <mr2dishes@gmail.com>
2024-08-27 21:30:43 +0200

    save

---
+++ sdl_main.cpp
@@ -0,0 +1,18 @@
+#include <lgfx/v1/platforms/sdl/Panel_sdl.hpp>
+#include "user_code.hpp"
+#if defined(SDL_h_)
+
+int userFunc(bool *running)
+{
+    setup();
+    do {
+      loop();
+    } while (*running);
+    return 0;
+}
+
+int main(int , char **) { 
+  return lgfx::Panel_sdl::main(userFunc); 
+}
+
+#endif
---
+++ user_code.hpp
@@ -0,0 +1,9 @@
+// user_code.h
+
+#ifndef USER_CODE_H
+#define USER_CODE_H
+
+void setup();
+void loop();
+
+#endif // USER_CODE_H
